d13c479cc1d22de0d6d2038924cdffb23590c66a
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-03-02 23:32:24 +0200

    fix 404 and 500 pages added

--- app/linkfloyd/urls.py
+++ app/linkfloyd/urls.py
@@ -6,6 +6,8 @@ from links.views import LinksListView, IndexView
 
 admin.autodiscover()
 
+from django.views.generic.simple import direct_to_template
+
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
@@ -15,7 +17,8 @@ urlpatterns = patterns('',
     url(r'^api/', include('linkfloyd.api.urls')),
     url(r'^accounts/', include('registration.urls')),
     url(r'^preferences/', include('preferences.urls')),
-    url(r'^invite/', include('privatebeta.urls')),
+    url(r'^404/$', direct_to_template, {'template': '404.html'}),
+
 )
 
 if settings.DEBUG:
