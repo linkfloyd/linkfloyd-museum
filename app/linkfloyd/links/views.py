ce06c9eed571c2ff69344980d1d38e22063bc6bc
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-03-17 14:20:47 +0200

    links from you...

--- app/linkfloyd/links/views.py
+++ app/linkfloyd/links/views.py
@@ -100,7 +100,7 @@ class IndexView(LinksListView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['listing_title'] = "Links from your subscripted channels"
+        context['listing_title'] = "Links From Your Subscripted Channels"
         return context
 
 
