cb576b55988af53cd2052beb765e92141d1aee03
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-04-08 01:42:16 +0300

    ...

--- app/linkfloyd/channels/models.py
+++ app/linkfloyd/channels/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
-
 class Channel(models.Model):
     """Channel model for linkfloyd.
 
