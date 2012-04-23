0fe5864e26d63714ee53ff22aab83f2caea25bc2
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-04-24 00:19:27 +0300

    ...

--- linkfloyd/fabfile.py
+++ linkfloyd/fabfile.py
@@ -5,7 +5,7 @@ def testing():
     env.host_string = "web77.webfaction.com"
     env.user = "miratcan"
     env.password = "kukuleta"
-    env.webapp = "/home/miratcan/webapps/linkfloyd/linkfloyd/app/linkfloyd"
+    env.webapp = "/home/miratcan/webapps/linkfloyd/linkfloyd/"
 
     env.activate_env = 'source /home/miratcan/envs/linkfloyd/bin/activate'
 
@@ -20,7 +20,7 @@ def pull():
 def update_libs():
     with prefix(env.activate_env):
         with cd(env.webapp):
-            run('pip install -r ../../docs/reqs.txt')
+            run('pip install -r ../../docs/requirements.txt')
             run('python manage.py collectstatic --noinput')
             run('python manage.py syncdb --noinput')
             run('../../../apache2/bin/restart')
