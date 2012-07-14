d9c7bfba292ab39054a5e43413ad4f0764afb576
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-07-14 12:56:15 +0300

    ...

--- linkfloyd/links/admin.py
+++ linkfloyd/links/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 from links.models import Link
 from links.models import Subscription
 from links.models import Report
-
-class LinkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'posted_by', 'posted_at', 'rating', 'shown')
+import datetime
 
 def mark_as_seen(modeladmin, request, queryset):
     queryset.update(seen=True)
 
+def mark_as_updated_now(modeladmin, request, queryset):
+    queryset.update(updated_at=datetime.datetime.now())
+
 mark_as_seen.short_description = "Mark selected reports as seen"
+mark_as_updated_now.short_description = "Mark selected links updated at now"
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'posted_by', 'posted_at', 'rating', 'shown')
+    actions = [mark_as_updated_now,]
 
 class ReportAdmin(admin.ModelAdmin):
     list_display = ("__unicode__", "reported_link", "note", "seen")
