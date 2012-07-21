b15c2951b8b16b3691637be93581f887af521bfb
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-07-22 01:29:22 +0300

    translation strings for preferences

--- linkfloyd/links/views.py
+++ linkfloyd/links/views.py
@@ -8,7 +8,6 @@ from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.models import User
 
-
 from channels.models import Subscription
 
 from links.models import Link, Channel
--- linkfloyd/preferences/models.py
+++ linkfloyd/preferences/models.py
@@ -5,29 +5,33 @@ from django.db.models.signals import post_save
 from links.models import SITE_RATINGS
 from django.db.utils import DatabaseError
 
+from django.utils.translation import ugettext as _
+
 class UserPreferences(models.Model):
     user = models.ForeignKey(User)
     description= models.CharField(
         max_length=255,
         blank=True,
         null=True,
-        help_text="describe yourself")
+        verbose_name=_("Description"),
+        help_text=_("describe yourself"))
     # known_languages = models.ManyToManyField(Language)
     max_rating = models.PositiveIntegerField(
-        verbose_name = "Maximum Rating",
+        verbose_name = _("Maximum Rating"),
         choices=SITE_RATINGS,
-        help_text="how much can you handle?",
+        help_text=_("how much can you handle?"),
         default=1
     )
     summary_mails = models.CharField(
         max_length=10,
         default="daily",
-        choices=(("daily", "Daily"),
-                 ("weekly", "Weekly"),
-                 ("monthly", "Monthly"),
-                 ("never", "Never Send")),
-        help_text="When do you want to get summaries of "
-                  "your subscripted channels",
+        choices=(("daily", _("Daily")),
+                 ("weekly", _("Weekly")),
+                 ("monthly", _("Monthly")),
+                 ("never", _("Never Send"))),
+        verbose_name=_("Summary Emails"),
+        help_text=_("When do you want to get summaries of " \
+                    "your subscripted channels")
     )
     def __unicode__(self):
         return "Preferences of %s" % self.user
@@ -36,10 +40,8 @@ def create_preferences(sender, instance, created, **kwargs):
     if created and UserPreferences.objects.filter(\
         user__username=instance.username).count() == 0:
         try:
-            preferences = UserPreferences.objects.create(
-                user=instance,
-                max_rating=1)
+            UserPreferences.objects.create(user=instance, max_rating=1)
         except DatabaseError:
-            preferences = None
+            pass
 
 post_save.connect(create_preferences, sender=User)
