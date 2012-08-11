2c4dac6bb3492299a507a48140bdb68dfbb52b31
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-08-12 00:50:35 +0300

    fix #65

--- linkfloyd/settings.py
+++ linkfloyd/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
   
 
