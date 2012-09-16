24c2e0c81039ef3c5519b5f97d08e0feeb335365
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-09-16 12:15:18 +0300

    secret_key.py local_settings.py ile birleştirildi, gerek yok iki farklı dosyaya

--- linkfloyd/settings.py
+++ linkfloyd/settings.py
@@ -61,9 +61,6 @@ STATICFILES_FINDERS = (
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-from secret_key import SECRET_KEY
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
