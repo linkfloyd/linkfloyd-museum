90d0b8e7efd052dd2f6757b0e4d03def213e85a5
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-12-26 14:53:01 +0200

    added some descriptive docstrings to context_builder.

--- linkfloyd/links/utils.py
+++ linkfloyd/links/utils.py
@@ -7,11 +7,49 @@ def get_in(d, k, p, df=None):
     return df
 
 def context_builder(request, **kwargs):
-    """Builds query via requestitem, if kwargs given overrides request.
-    """
-    from links.models import Link, Report, Channel
+
+    ''' (request object, [kwargs...]) -> response dict
+        
+    Builds query via request item contents overriden by kwargs.
+
+    links_from and instance properties
+    ----------------------------------
+
+    * Request object SHOULD HAVE "links_from" property as string which holds
+      type of what we are trying to list. For ex: if "links_from" is "user"
+      that means, we're going to list links from user.
+
+    * If "links_from" is "subscriptions", returns links from subscripted
+      channels of current user. If current user is not authenticated, returns
+      all links.
+
+    * If "links_from" is "user", returns links posted by user. "instance"
+      keyword supplied and it must an instance of User model.
+
+    * If "links_from" is "channel", returns links posted to channel.
+     "instance" keyword supplied and it must contain an instance of Channel
+     model.
+
+    * If "links_from" is "likes", returns links liked by user. "instance"
+      keyword supplied and it must contain an instance of User model.
+
+    * Request object can have "days" property which is integer, that limits
+      query in time. 
+
+    ordering and limiting
+    ---------------------
+
+    * Request object can be supplied with "days" property which is positive
+      integer to limit links in time.
+
+    * Request object can be supplied with "ordering" property which is 
+      string that can contain "contreversial", "top" or "latest" to get
+      links in that way.
+
+    '''
+    from links.models import Link, Report
     from preferences.models import UserPreferences
-    from channels.models import Channel, Subscription
+    from channels.models import Subscription
     from datetime import datetime
     from datetime import timedelta
     from django.core.paginator import Paginator
@@ -45,7 +83,9 @@ def context_builder(request, **kwargs):
     user_is_authenticated = request.user.is_authenticated()
 
     query = Q()
+
     if response['links_from'] == "subscriptions" and user_is_authenticated:
+        # TODO: this line can be optimised:
         query = query & Q(channel__in=[subscription.channel for \
             subscription in Subscription.objects.filter(user=request.user)])
     elif response['links_from'] == "channel":
@@ -53,6 +93,7 @@ def context_builder(request, **kwargs):
     elif response['links_from'] == "user":
         query = query & Q(posted_by=response['instance'])
     elif response['links_from'] == "likes":
+        # TODO: this line can be optimised:
         vote_model = get_vote_model('links.LinkVote')
         votes = vote_model.objects.filter(voter=response['instance'], value=1)
         query = query & Q(id__in = [vote.object.id for vote in votes])
@@ -68,14 +109,13 @@ def context_builder(request, **kwargs):
 
         # Filter links that not in known languages and rating higer than users.
         preferences = UserPreferences.objects.get(user=request.user)
-        query = query & \
-                Q(rating__lte  = preferences.max_rating)
+        query = query & Q(rating__lte  = preferences.max_rating)
 
     links = Link.objects.filter(query).order_by({
-    "hot": "-updated_at",
-        "controversial": "-comment_score",
-        "top": "-vote_score",
-        "latest": "-posted_at",
+        "hot": "-updated_at",
+            "controversial": "-comment_score",
+            "top": "-vote_score",
+            "latest": "-posted_at",
     }[response['ordering']])
 
     paginator = Paginator(links, 25)
