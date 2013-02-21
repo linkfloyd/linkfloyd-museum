f4f5e3140619fe57fc67f9af2cc33a18f751f9ce
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2013-02-21 18:28:57 +0200

    fix #119

--- linkfloyd/channels/views.py
+++ linkfloyd/channels/views.py
@@ -8,6 +8,8 @@ from django.contrib import messages
 from django.views.generic import ListView
 from django.db.models import Count
 from django.utils.translation import ugettext as _
+from channels.models import Language
+
 
 @login_required
 def create(request):
@@ -24,7 +26,7 @@ def create(request):
             messages.add_message(
                 request,
                 messages.SUCCESS,
-	        	'You Created %s Channel' % channel
+                'You Created %s Channel' % channel
             )
             return HttpResponseRedirect(channel.get_absolute_url())
         else:
@@ -40,19 +42,16 @@ def create(request):
             }, context_instance=RequestContext(request)
         )
 
+
 @login_required
 def update(request, slug):
     if request.POST:
         channel = get_object_or_404(Channel, slug=slug)
-        
-        get_object_or_404(
-            Subscription,
-            user=request.user,
-            channel=channel,
-            status="admin"
-        )
 
-        form = UpdateChannelForm(request.POST,instance=channel)
+        get_object_or_404(Subscription, user=request.user,
+            channel=channel, status="admin")
+
+        form = UpdateChannelForm(request.POST, instance=channel)
         if form.is_valid():
             channel = form.save()
             messages.add_message(
@@ -87,15 +86,24 @@ class BrowseChannelsView(ListView):
     template_name = "channels/channel_list.html"
 
     def get_queryset(self):
-        return Channel.objects.annotate(
-	        num_of_subscribers=Count("subscription")).order_by(
-            "-is_official", "-num_of_subscribers")
+        channels = Channel.objects.annotate(
+            num_of_subscribers=Count("subscription")).order_by(
+                "-is_official", "-num_of_subscribers")
+
+        code = self.request.GET.get('lang')
+        if code:
+            channels = channels.filter(language__code=code)
+
+        return channels
 
     def get_context_data(self, **kwargs):
         context = super(BrowseChannelsView, self).get_context_data(**kwargs)
         context['active_nav_item'] = "channels"
         context['title'] = _("Browsing Channels")
+        context['languages'] = Language.objects.all()
+        context['active_filter'] = self.request.GET.get('lang')
         return context
+
 
 class SubscriptionsView(BrowseChannelsView):
     def get_queryset(self):
